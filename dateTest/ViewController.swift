a6055eb7f5d680f1b532232cf66966457b5f3d66
Jack Ngai <jackngai@gmail.com>
2017-08-15 12:39:23 -0400

    Added Project code

--- dateTest/ViewController.swift
+++ dateTest/ViewController.swift
@@ -9,17 +9,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    var dateElements = DateComponents()
+    let unitFlags:Set<Calendar.Component> = [
+        .hour, .day, .month,
+        .year,.minute,.hour,.second,
+        .calendar]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        print(topOfTheHour())
+        print(getNowDate())
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func topOfTheHour()-> Date {
+        var now = Date()
+        var dateParts = DateComponents()
+        let calendar = Calendar.current
+        dateParts.year = Calendar.current.component(.year, from: now)
+        dateParts.month = Calendar.current.component(.month, from: now)
+        dateParts.day = Calendar.current.component(.day, from: now)
+        dateParts.hour = Calendar.current.component(.hour, from: now)
+        dateParts.minute = Calendar.current.component(.minute, from: now)
+        dateParts.second = Calendar.current.component(.second, from: now)
+        dateParts.timeZone = NSTimeZone.local
+        
+        if let minute = dateParts.minute, minute > 0{
+            dateParts.minute = 0
+            if let hour = dateParts.hour{
+                dateParts.hour = hour + 1
+            }
+        }
+        
+        now = calendar.date(from: dateParts)!
+        return now as Date
     }
-
-
+    
+    func getNowDate() -> Date {
+        return Date()
+    }
+    
+    
 }
 
